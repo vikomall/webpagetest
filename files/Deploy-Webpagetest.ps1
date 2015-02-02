b69edafef0411066a245cf044f72f4840eb94813
Justin Phelps <linuturk@onitato.com>
2015-02-02 14:15:20 -0600

    adding a user agent string

--- files/Deploy-Webpagetest.ps1
+++ files/Deploy-Webpagetest.ps1
@@ -70,6 +70,7 @@ Function Deploy-WebPagetest(){
         }
         Write-Log "[$(Get-Date)] Downloading $filename"
         $webclient = New-Object System.Net.WebClient;
+	$webclient.Headers.Add("user-agent", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)")
         $webclient.DownloadFile($url, $localpath + "\" + $filename)
     }
     function Unzip-File($fileName, $sourcePath, $destinationPath){
